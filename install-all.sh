ff438d37046acaacdcdb23742b37feec8cef2411
Adrian Eliasson <31988563+adrianeliasson@users.noreply.github.com>
2026-02-02 10:47:13 +0100

    Add install scripts

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+set -ex
+
+# Generate SSH key
+if [[ ! -f "${HOME}/.ssh/id_ed25519" ]]; then
+  # TODO: Generate SSH key here
+  echo "No default SSH key exists: ~/.ssh/id_ed25519"
+fi
+
+# General applications
 source ./install-ghostty.sh
+source ./install-thermald.sh
+source ./install-tmux.sh
+source ./install-brave.sh
+
+# Dev tools amd programming languages
+source ./install-go.sh
+source ./install-nvm.sh
+
+# Platform Engineer tools
+source ./install-kubectl.sh
+
+# Config files setup
 source ./install-stow.sh
 source ./install-dotfiles.sh
-source ./install-thermald.sh
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,6 +3,10 @@
 ORIGINAL_DIR=$(pwd)
 REPO_URL="https://github.com/adrianeliasson/.dotfiles"
 REPO_NAME=".dotfiles"
+REPO_DIR="${HOME}/.dotfiles"
+
+# TODO: Also check that SSH key is generated at correct spot,
+# and if not then query to generate ($HOME/.ssh/id_ed25519)
 
 is_stow_installed() {
   pacman -Qi "stow" &> /dev/null
@@ -19,7 +23,7 @@ cd "${HOME}"
 if [ -d "$REPO_NAME" ]; then
   echo "Repository '$REPO_NAME' already exists. Skipping clone"
 else
-  git clone "$REPO_URL"
+  git clone "$REPO_URL" "$REPO_DIR"
 fi
 
 if [ $? -ne 0 ]; then
@@ -27,20 +31,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "removing old configs"
-rm -rf \
-  ~/.config/starship.toml \
-  ~/.config/tmux/tmux.conf
-
-cd "$REPO_NAME"
-
-# Make backup of bashrc first
-mkdir -p "${HOME}/.backup"
-cp "${HOME}/.bashrc" "${HOME}/.backup/.bashrc.$(date +%s).bak"
-rm "${HOME}/.bashrc"
-# stow bashrc
-stow bash
-# stow ghostty
-stow tmux
-# stow nvim
-stow starship
+cd "${REPO_DIR}" && exec "${REPO_DIR}/install.sh"
